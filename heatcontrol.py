6ad44ace016a7694c68241bebd137b27c9360c3f
Koos van Strien <koos.van.strien@sigmadata.nl>
2022-03-04 21:03:58 +0100

    Code geschreven.

--- heatcontrol.py
+++ heatcontrol.py
@@ -1,28 +1,80 @@
 import RPi.GPIO as IO
+import re, os, time
+
+def read_sensor(path):
+  value = "U"
+  with open(path, "r") as f:
+    line = f.readline()
+    if re.match(r"([0-9a-f]{2} ){9}: crc=[0-9a-f]{2} YES", line):
+      line = f.readline()
+      m = re.match(r"([0-9a-f]{2} ){9}t=([+-]?[0-9]+)", line)
+      if m:
+        value = str(float(m.group(2)) / 1000.0)
+  return value
+
+# define paths to 1-wire sensor data
+paths = {
+  "tube": "/sys/bus/w1/devices/28-3c01d0750556/w1_slave",
+  "heatelement": "/sys/bus/w1/devices/28-3c01d075553b/w1_slave"
+}
 
 IO.setwarnings(False)
 IO.setmode(IO.BCM)
-IO.setup(11, IO.OUT)
+IO.setup(32, IO.OUT) # Pin 32 is GPIO 12
 
 # channel, frequency in Hz 
 # 0.5 = once every two seconds
-p = IO.PWM(11, 0.5) 
+p = IO.PWM(32, 10)
 
 
-
+# Sensors id's
+# 28-3c01d0750556  
+# 28-3c01d075553b
 
 ## Wat er moet gebeuren:
+doeltemp = input("Gewenste temperatuur (C):\n")
+max_temp = input("Maximale temperatuur op verwarmingselement (C):\n")
+minutes_to_cook = input("Aantal minuten dat doeltemperatuur aangehouden moet worden:\n")
 
-## vraag om input: temperatuur
-## vraag om input: maximale afwijking
-## Tijd op deze temperatuur: aantal minuten
+print("[Opwarmen] start")
 
-## OPWARMEN VOORAF
-## while huidigetemp_in_buis < doeltemp:
-##   if temp_op_sensor < max_temp en nog niet volledig aan:
-##     pwm puls verhogen (langere tijden aan)
-##   else:
-##     pwm puls verlagen (kortere tijden aan)
+dutycycle = 50
+olddutycycle = 50
+p.start(dutycycle)
+while read_sensor(paths["tube"]) < doeltemp:
+    olddutycycle = dutycycle
+    if read_sensor(paths["heatelement"]) < max_temp:
+        dutycycle = min(dutycycle + 10, 100)
+    else:
+        dutycycle = max(dutycycle - 10, 100)
+    if olddutycycle != dutycycle:
+        print("[Opwarmen] wijzig duty cycle naar {}".format(dutycycle))
+        p.ChangeDutyCycle(dutycycle)
+    time.sleep(3)
+
+print("[Opwarmen] voltooid")
+p.stop()
+
+start_cooking_time = time.time()
+end_cooking_time = time.time() + (minutes_to_cook * 60)
+
+
+
+print("[Koken] start")
+while time.time() < end_cooking_time:
+    olddutycycle = dutycycle
+    if read_sensor(paths["tube"]) < doeltemp and read_sensor(paths["heatelement"]) < max_temp:
+        dutycycle = min(dutycycle + 10, 100)
+    else:
+        # Als ofwel de doeltemp al bereikt is 
+        # of het verwarmingselement wordt te warm
+        # dan moet er altijd afgeschaald worden
+        # Uitschakelen gaat vanzelf (dutycycle = 0)
+        dutycycle = max(dutycycle - 10, 100)
+    if olddutycycle != dutycycle:
+        print("[Koken] wijzig duty cycle naar {}".format(dutycycle))
+        p.ChangeDutyCycle(dutycycle)
+    time.sleep(1)
 
 ## WARMHOUDEN
 ## end_time = huidigetijd() + aantal_minuten_koken
