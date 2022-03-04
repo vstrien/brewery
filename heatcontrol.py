6a1ec76333762845ec6a86fc28b5e1bc79049d76
Koos van Strien <koos.van.strien@sigmadata.nl>
2022-03-04 14:25:33 +0100

    Pseudocode opgezet (logica klopt nog niet 100%)

--- heatcontrol.py
+++ heatcontrol.py
@@ -2,9 +2,44 @@ import RPi.GPIO as IO
 
 IO.setwarnings(False)
 IO.setmode(IO.BCM)
-IO.setup(19, IO.OUT)
+IO.setup(11, IO.OUT)
 
 # channel, frequency in Hz 
 # 0.5 = once every two seconds
-p = IO.PWM(19, 0.5) 
+p = IO.PWM(11, 0.5) 
+
+
+
+
+## Wat er moet gebeuren:
+
+## vraag om input: temperatuur
+## vraag om input: maximale afwijking
+## Tijd op deze temperatuur: aantal minuten
+
+## OPWARMEN VOORAF
+## while huidigetemp_in_buis < doeltemp:
+##   if temp_op_sensor < max_temp en nog niet volledig aan:
+##     pwm puls verhogen (langere tijden aan)
+##   else:
+##     pwm puls verlagen (kortere tijden aan)
+
+## WARMHOUDEN
+## end_time = huidigetijd() + aantal_minuten_koken
+## while huidigetijd() < aantal_minuten_koken:
+##   if huidigetemp_in_buis < doeltemp or temp_op_sensor < doeltemp:
+##     if temp_op_sensor < max_temp:
+##       pwm puls verhogen (langere tijden aan)
+##     else:
+##       sensor uit
+## 
+## Doeltemperatuur instellen en maximale afwijking
+
+## Temperatuur niet bereikt?
+## Temperatuuropgang inzetten, maximaal vermogen
+
+## Controleren of het element niet meer dan de maximale afwijking vertoont
+
+## Bij bereiken temperatuur: timer starten
+## Bij aflopen timer: nieuwe temperatuur vragen
 
