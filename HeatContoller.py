ac1eb3b7164db27d4927c57884c31911d54f5b86
Koos van Strien <koos.van.strien@sigmadata.nl>
2023-12-27 15:14:47 +0100

    Continouous setup (multi-threaded)

---
+++ HeatContoller.py
@@ -0,0 +1,79 @@
+import RPi.GPIO as IO
+import re
+import threading
+import time
+
+class HeatController:
+  def __init__(self, tube_path, heat_path, target_temperature = 0, max_temperature = 0, GPIO_PIN = 18):
+    IO.setwarnings(False)
+    IO.setmode(IO.BCM)
+    IO.setup(GPIO_PIN, IO.OUT) # Pin 12 is GPIO 18
+    self.target_lock = threading.Lock()
+    self.sensor_lock = threading.Lock()
+    self.target_temperature = target_temperature
+    self.max_temperature = max_temperature
+    self.tubetemp = 0
+    self.elemtemp = 0
+    self.dutycycle = 0
+    self.p = IO.PWM(18, 10)
+    self.tube_path = tube_path
+    self.heat_path = heat_path
+    self.margin = 2 # Margin in degrees celcius
+
+  @staticmethod
+  def read_sensor(path):
+    value = "U"
+    with open(path, "r") as f:
+        line = f.readline()
+        if re.match(r"([0-9a-f]{2} ){9}: crc=[0-9a-f]{2} YES", line):
+            line = f.readline()
+            m = re.match(r"([0-9a-f]{2} ){9}t=([+-]?[0-9]+)", line)
+            if m:
+                value = float(m.group(2)) / 1000.0
+    return value
+
+  def updateSensorValues(self):
+    with self.sensor_lock:
+        self.tubetemp = HeatController.read_sensor(self.tube_path)
+        self.elemtemp = HeatController.read_sensor(self.heat_path)
+
+  def setTargetTemperature(self, target_temperature):
+    with self.target_lock:
+        self.target_temperature = target_temperature
+
+  def setMaxTemperature(self, max_temperature):
+    with self.target_lock:
+        self.max_temperature = max_temperature
+
+  def updateDutyCycle(self):
+     self.updateSensorValues()
+     current_max = max(self.tubetemp, self.elemtemp)
+     with self.target_lock:
+        if self.target_temperature < current_max - self.margin:
+            # Below the allowed margin. Full power.
+            self.dutycycle = 100
+        elif self.target_temperature > current_max + self.margin:
+            # Above the allowed margin. Turn off.
+            self.dutycycle = 0
+        else:
+            # Between the allowed margin. Half power.
+            # In the future, we could fine-tune this
+            self.dutycycle = 50
+
+  def updateDutyCycleDaemon(self, seconds_between_updates = 30):
+    while True:
+        self.updateDutyCycle()
+        time.sleep(seconds_between_updates)
+
+  def start(self):
+    self.updateDutyCycle()
+    p.start(self.dutycycle)
+
+    # Start the update thread
+    self.update_thread = threading.Thread(target=self.updateDutyCycleDaemon)
+    self.update_thread.daemon = True
+    self.update_thread.start()
+     
+  def stop(self):
+    p.stop()
+    self.update_thread.join(timeout=0)
---
+++ main.py
@@ -0,0 +1,34 @@
+"""
+Continuous heat control for the brewery.
+After entering the desired temperature and cook time, the program will keep this in the background.
+This allows the operator to change the temperature at any given time.
+"""
+
+from HeatContoller import HeatController
+
+# Load settings
+import configparser
+config = configparser.ConfigParser()
+config.read('settings.ini')
+
+heatelement = config['devices']['heatelement']
+tube = config['devices']['tube']
+
+hc = HeatController(tube, heatelement)
+
+# Get user input
+while True:
+    print("Enter the desired temperature (C):")
+    print("Enter STOP to stop the program")
+    target_temperature = input("Enter the desired temperature (C):\n")
+    if target_temperature.lower == "stop":
+        hc.stop()
+        break
+    else:
+        try:
+            target_temperature = int(target_temperature)
+            hc.setTargetTemperature(target_temperature)
+        except ValueError:
+            print("Invalid input")
+            continue
+
